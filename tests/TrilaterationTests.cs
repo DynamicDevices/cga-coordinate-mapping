b42ba088fc70b5efaff4d44ef37bd93c91350912
Alex J Lennon <ajlenon@dynamicdevices.co.uk>
2025-11-14 10:27:37 -0300

    Fix CI: Add using Xunit to remaining test files
    - Added 'using Xunit;' to AppConfigTests, TrilaterationTests, and UWB2GPSConverterTests
    - All test files now have explicit using statements
    - MSBuild Using items are not reliable in CI
    - All 92 tests pass

--- tests/TrilaterationTests.cs
+++ tests/TrilaterationTests.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
+using Xunit;
 
 namespace InstDotNet.Tests;
 
--- tests/UWB2GPSConverterTests.cs
+++ tests/UWB2GPSConverterTests.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Numerics;
+using Xunit;
 
 namespace InstDotNet.Tests;
 
